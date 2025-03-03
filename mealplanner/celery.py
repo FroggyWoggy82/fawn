2f9f77dff6ccb05f31573bc622bb968650715531
Kevin <kevinguyen022@gmail.com>
2025-03-03 13:25:34 -0600

    updatedcelery.py

--- mealplanner/celery.py
+++ mealplanner/celery.py
@@ -6,29 +6,11 @@ from celery import Celery
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mealplanner.settings')
 
-# Create the Celery app and specify the broker URL
-app = Celery('mealplanner', broker='redis://localhost:6379/0')
-
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a 'CELERY_' prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
-
-# Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
-
-app.conf.beat_schedule = {
-    'send-daily-submission-email': {
-        'task': 'meals.tasks.send_daily_submission_email',
-        'schedule': crontab(hour=0, minute=0),  # Adjust the schedule as needed
-    },
-}
-
+app = Celery('mealplanner')
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mealplanner.settings')
 
-app = Celery('mealplanner')
+
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
