6d5bc3cf30091954fbdba01d0269535545695650
Kevin <kevinguyen022@gmail.com>
2025-03-06 10:20:41 -0600

    morefixes

--- meals/context_processors.py
+++ meals/context_processors.py
@@ -1,6 +1,10 @@
+from django.conf import settings
 import os
 
 def vapid_key(request):
+    vapid_key = os.environ.get('VAPID_PUBLIC_KEY', '')
+    if not vapid_key:
+        print("Warning: VAPID_PUBLIC_KEY not found in environment variables")
     return {
-        'vapid_public_key': os.environ.get('VAPID_PUBLIC_KEY', '')
+        'vapid_public_key': vapid_key
     }
